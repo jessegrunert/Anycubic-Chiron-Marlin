5c6fbb27b515e348841062b6d8a276af6e19ef29
Jesse Grunert <jgrunert@acromedia.com>
2019-05-25 11:05:24 -0700

    disabled linear advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -631,7 +631,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 50, 30000 } //{ 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1400, 1400, 50, 30000 } //{ 3000, 3000, 100, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -641,9 +641,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1500 //3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          1400 //3000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1500 //3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   1400 //3000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -653,8 +653,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 15 //10.0
-#define DEFAULT_YJERK                 15 //10.0
+#define DEFAULT_XJERK                 10 //10.0
+#define DEFAULT_YJERK                 10 //10.0
 #define DEFAULT_ZJERK                 0.4 //0.3
 #define DEFAULT_EJERK                 20  //5.0
 
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -774,7 +774,7 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   #define LIN_ADVANCE_K 1.4   // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG          // If enabled, this will generate debug information output over USB.
